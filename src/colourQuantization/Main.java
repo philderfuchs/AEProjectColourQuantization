dfb8a3a6ef10cc1c3572709326ab6529a84614c6
Philipp Anders <philipp.anders.pa@gmail.com>
2015-11-19 18:44:20 +0100

    pixel data read succesfully

--- src/colourQuantization/Main.java
+++ src/colourQuantization/Main.java
@@ -1,12 +1,46 @@
 package colourQuantization;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Main {
 
-	public static final String imgSource = "matty.jpg";
+	public static final String imgSource = "resources/samplePalette.png";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 BufferedImage img;
+		 
+	        try {
+				img = ImageIO.read(new File(imgSource));
+				System.out.println("Image read succesfully");
+				System.out.println(img.getHeight());
+				System.out.println(img.getWidth());
+				
+				int imgHeight = img.getHeight();
+				int imgWidth = img.getWidth();
+				
+				Color[] pixelData = new Color[imgHeight * imgWidth];
+				int arrayCounter = 0;
+				
+				for(int y = 0; y < imgHeight; y++) {
+					for(int x = 0; x < imgWidth; x++) {
+						pixelData[arrayCounter++] = new Color(img.getRGB(x, y));
+					}
+				}
+				
+				for (Color c : pixelData){
+					System.out.println("(" + c.getRed() + ", " + c.getGreen() + ", " + c.getBlue() + ")");
+				}
+				
+			} catch (IOException e) {
+				System.err.println("Error while Reading Image");
+				e.printStackTrace();
+			}
+		 
 	}
 
 }
