8217c4b29cc75e1a57046843c2ad2aeca3b3dfe3
Philipp Anders <philipp.anders.pa@gmail.com>
2015-11-19 18:05:10 +0100

    init commit

---
+++ src/colourQuantization/Main.java
@@ -0,0 +1,12 @@
+package colourQuantization;
+
+public class Main {
+
+	public static final String imgSource = "matty.jpg";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
